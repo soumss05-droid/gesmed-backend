3f1b445ecbd4d9414bf5b20b57e7e5ed5003a541
Amedy Soumare <toi@example.com>
2026-09-26 01:39:49 +0000

    Supprime un fichier de routes produits dupliqué et obsolete a la racine du projet

--- produits.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const { authentifier } = require("../middleware/auth");
-const { listerProduits } = require("../controllers/produits.controller");
-
-const router = express.Router();
-
-router.get("/", authentifier, listerProduits);
-
-module.exports = router;
